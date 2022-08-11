d51b65070a9fe3ae7db7a466fb6d4c32b09d7a90
PopovaElis <elizprest@gmail.com>
2022-08-11 22:51:14 +0500

    Вся домашка

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-   //Задание 1
     public static void main(String[] args) {
+        //Задание 1
          int i = 1;
         while (i < 11) {
             System.out.print(i + " ");;
@@ -11,5 +11,22 @@ public class Main {
             for(int a = 10;  a >= 1; a -- ){
                 System.out.print(a + " ");
 }
+//Задание 2
+        System.out.println( );
+        System.out.println( );
+    int day= 3;
+      for(int b = day;  b <= 31; b=b+7 ){
+        System.out.println("Сегодня пятница, " + b +  "-е число. Необходимо подготовить отчет.");
+    }
+      //Задание 3
+        System.out.println();
+        int c= 1820;
+        int d =2120;
+        for (int e = 0; e <=d; e=e+79)
+            if(e>c){
+                System.out.println(e);
+
 }
 }
+}
+
